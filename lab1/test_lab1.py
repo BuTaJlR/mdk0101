0454fca062680079600a83ed1708e1b737e2d7b6
BuTaJlR <65667345+BuTaJlR@users.noreply.github.com>
2022-10-15 13:59:52 +0300

    Update test_lab1.py

--- lab1/test_lab1.py
+++ lab1/test_lab1.py
@@ -7,3 +7,7 @@ class TestLab(unittest.TestCase):
         self.assertEqual(lab1.ZeroCount(0), 1)
         self.assertEqual(lab1.ZeroCount(101), 1)
         self.assertEqual(lab1.ZeroCount(100), 2)
+
+        
+if __name__ == '__main__':
+    unittest.main()
