c55a9db339480f5cd2a7475c31dacbc67c05487e
BuTaJlR <65667345+BuTaJlR@users.noreply.github.com>
2022-10-29 14:49:24 +0300

    1

--- lab3/script.sql
+++ lab3/script.sql
@@ -3,12 +3,14 @@ drop table if exists Personal;
 drop table if exists Ticket;
 drop table if exists Excursion;
 drop table if exists Museum;
+drop table if exists Exhibit;
+drop table if exists Exhibit_expansion;
 
 create table User(
-	id integer primary key autoincrement,
-	number varchar(12) not null ,
-	name varchar(20) not null,
-	surname varchar(20) not null
+	id integer PRIMARY KEY autoincrement,
+	number varchar(12) NOT NULL ,
+	name varchar(20) NOT NULL,
+	surname varchar(20) NOT NULL
 );
 
 create table Personal(
@@ -33,11 +35,30 @@ create table Excursion(
 	date_start date,
 	museumId integer not null,
 	presonalId integer not null,
+	exhibitId integer not null,
 	foreign key (museumId) references Museum(id)	
 	foreign key (presonalId) references Personal(id)
+	foreign key (exhibitId) references Exhibit_expansion(exhibitId)
 );
 
 create table Museum(
 	id integer primary key autoincrement,
-	name varchar(256) not null
+	name varchar(256) not null,
+	exhibitId integer not null
+);
+
+create table Exhibit(
+	id integer primary key autoincrement,
+	museumId integer not null,
+	name varchar(20),
+	discription varchar(256),
+	foreign key (museumId) references Museum(id)
+);
+
+create table Exhibit_expansion(
+	id integer primary key autoincrement,
+	exhibitId integer not null,
+	excursionId integer not null,
+	foreign key (exhibitId) references Exhibit(id)
+	foreign key (excursionId) references Excursion(id)
 );
