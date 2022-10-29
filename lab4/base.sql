d0cde972decf9038cb2d589be41277fe120829d2
BuTaJlR <65667345+BuTaJlR@users.noreply.github.com>
2022-10-29 14:50:15 +0300

    1

--- lab4/base.sql
+++ lab4/base.sql
@@ -28,22 +28,45 @@ create table Excursion(
 	date_start date,
 	museumId integer not null,
 	presonalId integer not null,
+	exhibitId integer not null,
 	foreign key (museumId) references Museum(id)	
 	foreign key (presonalId) references Personal(id)
+	foreign key (exhibitId) references Exhibit_expansion(exhibitId)
 );
 
 create table Museum(
 	id integer primary key autoincrement,
-	name varchar(256) not null
+	name varchar(256) not null,
+	exhibitId integer not null
 );
 
-insert into Museum (name) 
-	values ("Historical Museum"), ("Weapons Museum");
+create table Exhibit(
+	id integer primary key autoincrement,
+	museumId integer not null,
+	name varchar(20),
+	discription varchar(256),
+	foreign key (museumId) references Museum(id)
+);
+
+create table Exhibit_expansion(
+	id integer primary key autoincrement,
+	exhibitId integer not null,
+	excursionId integer not null,
+	foreign key (exhibitId) references Exhibit(id)
+	foreign key (excursionId) references Excursion(id)
+);
+
+insert into Museum (name, exhibitId) 
+	values ("Historical Museum",1), ("Weapons Museum",1);
 insert into User (name, surname, number)
 	values ("UserName1", "UserSurname1", "1234567890"), ("UserName2", "UserSurname2", "1253567892");
 insert into Personal (name, surname, number, power_level) 
 	values ('PresonalName1', 'presonalSurname1', '88055598233', 1);
-insert into Excursion (museumId, presonalId, date_start) 
-	values (1, 1,'2020-20-02'), (2, 2,'2020-20-02');
+insert into Excursion (museumId, presonalId, date_start, exhibitId) 
+	values (1, 1,'2020-20-02',1), (2, 2,'2020-20-02',1);
 insert into Ticket (excursionId, userId, price) 
-	values(1, 1, 2500),(2, 2, 2500)
+	values(1, 1, 2500),(2, 2, 2500);
+insert into Exhibit(museumId, name, discription)
+	values(1,"exName1","exDiscriptrion");
+insert into Exhibit_expansion(exhibitId, excursionId)
+	values(1,1);
